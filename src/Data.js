b61efdafabd96f99fb0284e309196f9fbb3af8f0
santude1997 <santude1997@outlook.com>
2023-12-06 18:43:29 +0530

    added code in remort repo

---
+++ src/Data.js
@@ -0,0 +1,12 @@
+export const userList = [
+  {
+    name: "your name",
+    email: "your@email.com",
+    id: 1,
+  },
+  {
+    name: "my name",
+    email: "myemail@email.com",
+    id: 2,
+  },
+];
---
+++ src/Home.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { deleteUser } from "./UserReducer";
+
+const Home = () => {
+  const users = useSelector((state) => state.users);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleDelete = (id) => {
+    dispatch(deleteUser({ id: id }));
+    navigate("/");
+  };
+  console.log(users);
+  return (
+    <div className="container">
+      <h2>CURD APP By using JSON Server</h2>
+      <Link to="/create" className="btn btn-success my-3">
+        Create +
+      </Link>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, index) => (
+            <tr key={index}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>
+                <Link
+                  to={`/edit/${user.id}`}
+                  className="btn btn-sm btn-primary"
+                >
+                  Edit
+                </Link>
+                <Link
+                  onClick={() => handleDelete(user.id)}
+                  className="btn btn-sm btn-danger ms-2"
+                >
+                  Delete
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Home;
